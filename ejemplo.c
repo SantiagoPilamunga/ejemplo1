1a970609cef7b206eb2ef798396316aff01c474d
Santiago <santiago.pilamunga@udla.edu.ec>
2023-10-19 16:31:26 -0500

    trabajo

--- ejemplo.c
+++ ejemplo.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+int suma(int a, int b)
+{
+    int res=a+b;
+    return res;
+}
+
 int main()
 {
-    
+    int num1,num2,resultado=0;
+    printf("Ingrese el primer valor: ");
+    scanf("%d", &num1);
+        printf("Ingrese el segundo valor: ");
+    scanf("%d", &num2);
+    resultado=suma(num1,num2);
+    printf("Resultado %d",resultado);
     return 0;
 }
